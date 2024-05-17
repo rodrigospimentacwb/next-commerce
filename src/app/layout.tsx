291f74490bf871f6e896dd94504923f19b957d32
rodrigospimentacwb <rodrigospimenta@gmail.com>
2024-05-17 10:49:47 -0300

    Criado Componente NavBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Navbar from './components/Navbar';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Next E-Commerce 14",
-  description: "Next E-Commerce utilizando a versão 14",
+  title: 'Next E-Commerce 14',
+  description: 'Next E-Commerce utilizando a versão 14',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <Navbar />
+      <body className={inter.className}>
+        <main className='bg-slate-700 h-screen p-16'>{children}</main>
+      </body>
     </html>
   );
 }

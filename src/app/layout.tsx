cee56784a5cc91a391f92d22e080ce4520c7de78
rodrigospimentacwb <rodrigospimenta@gmail.com>
2024-05-17 18:58:40 -0300

    Adicionado autenticacao com clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { ptBR } from '@clerk/localizations'
+import { ClerkProvider } from '@clerk/nextjs'
 import clsx from 'clsx'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -13,11 +15,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='en'>
-      <body className={clsx(inter.className, 'bg-slate-700')}>
-        <Navbar />
-        <main className='h-screen p-16'>{children}</main>
-      </body>
-    </html>
+    <ClerkProvider localization={ptBR}>
+      <html lang='en'>
+        <body className={clsx(inter.className, 'bg-slate-700')}>
+          <Navbar />
+          <main className='h-screen p-16'>{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
